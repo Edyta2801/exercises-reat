40638e09cfd1ee2a693229947c5d42885e60dd66
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-12-02 14:56:13 +0100

    BrowserRouter as Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Route1 from './Route1'
 import Route2 from './Route2'
 import Route3 from './Route3'
@@ -8,7 +8,7 @@ import Button from './Button'
 
 const App = (props) => (
   <div>
-    <BrowserRouter>
+    <Router>
       <div>
         <Route path="/" exact={true} component={()=>
         <Button label="NIC!"/>}/>
@@ -16,7 +16,7 @@ const App = (props) => (
         <Route path="/route-2" component={Route2}/>
         <Route path="/route-3" component={Route3}/>
       </div>
-    </BrowserRouter>
+    </Router>
   </div>
 );
 
