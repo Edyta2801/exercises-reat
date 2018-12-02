3c5682027a9382ae24bcfd0b3a7f56b4d749405f
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-12-02 14:23:36 +0100

    add 3 router comonents

---
+++ src/Route2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Route1= (props)=>(
+    <h1>
+        Route2
+    </h1>
+
+
+export default Route2
---
+++ src/Route3.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Route1= (props)=>(
+    <h1>
+        Route3
+    </h1>
+
+
+export default Route3
