58b520ea703b47d5232616db3b1801dee0d4f47e
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-12-02 15:46:42 +0100

    Nested Routing

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import Route1 from './Route1'
-import Route2 from './Route2'
-import Route3 from './Route3'
-import Button from './Button'
-import DisplayParam from './DisplayParam'
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+const App = () => (
+  <Router>
+    <div>
+      <Header />
 
-const App = (props) => (
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/topics" component={Topics} />
+    </div>
+  </Router>
+);
+
+const Home = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
+const Topic = ({ match }) => <h3>Requested Param: {match.params.id}</h3>;
+const Topics = ({ match }) => (
   <div>
-    <Router>
-      <div>
-        <div>
-          <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/route-1'>Route1</Link></li>
-          <li><Link to='/route-2'>Route2</Link></li>
-          <li><Link to='/route-3'>Route3</Link></li>
-          </ul>
-      </div>
-        <Route path="/" exact={true} component={()=>
-        <Button label="NIC!"/>}/>
-        <Route path="/route-1" component={Route1}/>
-        <Route path="/route-2" component={Route2}/>
-        <Route path="/route-3" component={Route3}/>
-        <Route path="/param/:key" component={DisplayParam}/>
-      </div>
-    </Router>
+    <h2>Topics</h2>
+
+    <ul>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.path}/:id`} component={Topic} />
+    <Route
+      exact
+      path={match.path}
+      render={() => <h3>Please select a topic.</h3>}
+    />
   </div>
+);
+const Header = () => (
+  <ul>
+    <li>
+      <Link to="/">Home</Link>
+    </li>
+    <li>
+      <Link to="/about">About</Link>
+    </li>
+    <li>
+      <Link to="/topics">Topics</Link>
+    </li>
+  </ul>
 );
 
 export default App;
