a2cbc1fb084fadb6b777feedc146c6422849e5b2
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-12-02 17:36:43 +0100

    RaisedButton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import RaisedButton from 'material-ui/RaisedButton'
 
+const style = { margin: 12 }
 
 const App = (props) => (
-    <div>
-      App
-    </div>
+  <div>
+    <RaisedButton
+      label="Primary"
+      primary={true}
+      style={style}
+    />
+  </div>
 );
 
 
